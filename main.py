1eb4b93827be36c2ff7e184b4434c141ef462dd6
AlixCORBIN <alixc72430@gmail.com>
2025-10-27 19:22:36 +0100

    Reduction de FRONT_CONTROL 160 --> 150

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ FONT_SCORE = QFont("Arial Black", 200)
 FONT_TEAM = QFont("Arial Black", 70)
 FONT_TM = QFont("Arial", 30)
 FONT_CHRONO = QFont("Arial Black", 200)
-FONT_CONTROL = QFont("Arial",160)
+FONT_CONTROL = QFont("Arial",150)
 FONT_PERIODE = QFont("Arial Black", 70)
 FONT_NUM = QFont("Arial", 28)
 
