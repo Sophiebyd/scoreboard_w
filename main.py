ba576c506cbbe5671d62a2a658e2181c491a4ca0
Sophiebyd <sophiebyd@proton.me>
2025-10-28 15:51:41 +0100

    nettoyage structure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from ui_display import DisplayWindow
-from ui_control import ControlWindow
+from ui.ui_display import DisplayWindow
+from ui.ui_control import ControlWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
